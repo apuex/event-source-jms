5e545e73d868e0fb5673a68fb11e61b757109831
Wangxy <xtwxy@hotmail.com>
2018-09-18 19:04:34 +0800

    add principal to interface.

--- src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
+++ src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
@@ -3,6 +3,8 @@ package com.github.apuex.eventsource.jms;
 import com.github.apuex.eventsource.EventSourceAdapter;
 import org.springframework.jms.core.JmsTemplate;
 
+import java.security.Principal;
+
 public class EventSourceJmsAdapter implements EventSourceAdapter {
   private final JmsTemplate jmsTemplate;
 
@@ -13,5 +15,13 @@ public class EventSourceJmsAdapter implements EventSourceAdapter {
   @Override
   public void publish(Object o) {
     jmsTemplate.convertAndSend(o);
+  }
+
+  @Override
+  public void publish(Object event, Principal principal) {
+    jmsTemplate.convertAndSend(event, message -> { if(null != principal)
+      message.setStringProperty("principalName", principal.getName());
+      return message;
+    });
   }
 }
