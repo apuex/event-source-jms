d0a06fd1aa2e1a121a0731bf3bacea94b1918277
apuex <93236565@qq.com>
2018-09-19 22:47:42 +0800

    refactoring resourceUri to serviceUri.

--- src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
+++ src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
@@ -2,7 +2,7 @@ package com.github.apuex.eventsource.jms;
 
 import com.github.apuex.eventsource.EventSourceAdapter;
 import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_PRINCIPAL_NAME_FIELD;
-import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_RESOURCE_URI_FIELD;
+import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_SERVICE_URI_FIELD;
 
 import org.springframework.jms.core.JmsTemplate;
 
@@ -11,7 +11,7 @@ import java.security.Principal;
 
 public class EventSourceJmsAdapter implements EventSourceAdapter {
   private String principalNameField = DEFAULT_PRINCIPAL_NAME_FIELD;
-  private String resourceUriField = DEFAULT_RESOURCE_URI_FIELD;
+  private String serviceUriField = DEFAULT_SERVICE_URI_FIELD;
   private JmsTemplate jmsTemplate;
 
   @Override
@@ -28,10 +28,10 @@ public class EventSourceJmsAdapter implements EventSourceAdapter {
   }
 
   @Override
-  public void publish(Object event, Principal principal, URI uri) {
+  public void publish(Object event, Principal principal, URI service) {
     jmsTemplate.convertAndSend(event, message -> { if(null != principal)
       message.setStringProperty(principalNameField, principal.getName());
-      message.setStringProperty(resourceUriField, uri.toString());
+      message.setStringProperty(serviceUriField, service.toString());
       return message;
     });
   }
@@ -40,8 +40,8 @@ public class EventSourceJmsAdapter implements EventSourceAdapter {
     this.principalNameField = principalNameField;
   }
 
-  public void setResourceUriField(String resourceUriField) {
-    this.resourceUriField = resourceUriField;
+  public void setServiceUriField(String serviceUriField) {
+    this.serviceUriField = serviceUriField;
   }
 
   public void setJmsTemplate(JmsTemplate jmsTemplate) {
