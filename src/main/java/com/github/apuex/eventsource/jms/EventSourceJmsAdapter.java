959e91391913b03f11d47acbd7178fc16b9d0fee
Wangxy <xtwxy@hotmail.com>
2018-09-19 10:36:27 +0800

    add security principal to message.

--- src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
+++ src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
@@ -1,11 +1,13 @@
 package com.github.apuex.eventsource.jms;
 
 import com.github.apuex.eventsource.EventSourceAdapter;
+import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_PRINCIPAL_NAME_FIELD;
 import org.springframework.jms.core.JmsTemplate;
 
 import java.security.Principal;
 
 public class EventSourceJmsAdapter implements EventSourceAdapter {
+  private String principalNameField = DEFAULT_PRINCIPAL_NAME_FIELD;
   private final JmsTemplate jmsTemplate;
 
   public EventSourceJmsAdapter(JmsTemplate jmsTemplate) {
