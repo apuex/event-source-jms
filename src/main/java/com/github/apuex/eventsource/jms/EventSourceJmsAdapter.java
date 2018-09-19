e36a1128cf795e8463a6cdf05cb797e461983d1f
Wangxy <xtwxy@hotmail.com>
2018-09-19 17:37:51 +0800

    add resource uri to message listener.

--- src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
+++ src/main/java/com/github/apuex/eventsource/jms/EventSourceJmsAdapter.java
@@ -2,17 +2,17 @@ package com.github.apuex.eventsource.jms;
 
 import com.github.apuex.eventsource.EventSourceAdapter;
 import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_PRINCIPAL_NAME_FIELD;
+import static com.github.apuex.protobuf.jms.JmsConverterConfig.DEFAULT_RESOURCE_URI_FIELD;
+
 import org.springframework.jms.core.JmsTemplate;
 
+import java.net.URI;
 import java.security.Principal;
 
 public class EventSourceJmsAdapter implements EventSourceAdapter {
   private String principalNameField = DEFAULT_PRINCIPAL_NAME_FIELD;
-  private final JmsTemplate jmsTemplate;
-
-  public EventSourceJmsAdapter(JmsTemplate jmsTemplate) {
-    this.jmsTemplate = jmsTemplate;
-  }
+  private String resourceUriField = DEFAULT_RESOURCE_URI_FIELD;
+  private JmsTemplate jmsTemplate;
 
   @Override
   public void publish(Object o) {
@@ -22,8 +22,29 @@ public class EventSourceJmsAdapter implements EventSourceAdapter {
   @Override
   public void publish(Object event, Principal principal) {
     jmsTemplate.convertAndSend(event, message -> { if(null != principal)
-      message.setStringProperty("principalName", principal.getName());
+      message.setStringProperty(principalNameField, principal.getName());
       return message;
     });
+  }
+
+  @Override
+  public void publish(Object event, Principal principal, URI uri) {
+    jmsTemplate.convertAndSend(event, message -> { if(null != principal)
+      message.setStringProperty(principalNameField, principal.getName());
+      message.setStringProperty(resourceUriField, uri.toString());
+      return message;
+    });
+  }
+
+  public void setPrincipalNameField(String principalNameField) {
+    this.principalNameField = principalNameField;
+  }
+
+  public void setResourceUriField(String resourceUriField) {
+    this.resourceUriField = resourceUriField;
+  }
+
+  public void setJmsTemplate(JmsTemplate jmsTemplate) {
+    this.jmsTemplate = jmsTemplate;
   }
 }
